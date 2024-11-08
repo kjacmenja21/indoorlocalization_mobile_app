ea6861ca7da8a4e9bf4e49828f827e19933e26bd
jmojzes21 <jmojzes21@student.foi.hr>
2024-11-08 18:19:38 +0100

    Add il_entities package

---
+++ packages/il_entities/example/il_entities_example.dart
@@ -0,0 +1,6 @@
+import 'package:il_entities/il_entities.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/il_entities/test/il_entities_test.dart
@@ -0,0 +1,16 @@
+import 'package:il_entities/il_entities.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
