35e71a734b3ae639e93129ae470742c332fabd8d
jmojzes21 <jmojzes21@student.foi.hr>
2024-11-16 18:11:21 +0100

    Update basic login handler to use auth service

--- il_basic_auth/lib/src/basic_login_handler.dart
+++ il_basic_auth/lib/src/basic_login_handler.dart
@@ -1,33 +1,26 @@
 import 'package:il_basic_auth/il_basic_auth.dart';
 import 'package:il_core/il_core.dart';
-import 'package:il_entities/il_entities.dart';
+import 'package:il_ws/il_ws.dart';
 
 class BasicLoginHandler extends LoginHandler {
   @override
   void handleLogin({
     required LoginToken baseToken,
     required LoginOutcomeListener loginListener,
-  }) {
+  }) async {
     if (baseToken is BasicLoginToken == false) {
       throw ArgumentError("Must receive BasicLoginToken instance");
     }
 
     BasicLoginToken token = baseToken as BasicLoginToken;
 
-    if (token.username == "admin" && token.password == "admin") {
-      loginListener.onSuccessfulLogin(
-        User(
-          id: 0,
-          username: 'admin',
-          firstName: '',
-          lastName: '',
-          email: '',
-          contact: '',
-          role: UserRole(id: 0, name: ''),
-        ),
-      );
-    } else {
-      loginListener.onFailedLogin("Invalid username or password.");
+    try {
+      var authService = AuthenticationService();
+      var user = await authService.login(token.username, token.password);
+
+      loginListener.onSuccessfulLogin(user);
+    } on AppException catch (e) {
+      loginListener.onFailedLogin(e.message);
     }
   }
 }
