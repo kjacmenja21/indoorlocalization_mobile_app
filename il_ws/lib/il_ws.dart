c93f16361680ce71442e1d4cc71ced0091e2fe28
jmojzes21 <jmojzes21@student.foi.hr>
2024-11-19 20:46:45 +0100

    Add Backend context and backend mdns discovery

--- il_ws/lib/il_ws.dart
+++ il_ws/lib/il_ws.dart
@@ -1,3 +1,5 @@
 library;
 
 export 'src/services/authentication_service.dart';
+export 'src/context/backend_context.dart';
+export 'src/misc/backend_discovery.dart';
---
+++ il_ws/lib/src/misc/backend_discovery.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:multicast_dns/multicast_dns.dart';
+
+class BackendDiscovery {
+  Future<String> discoverBackendAddress() async {
+    var completer = Completer<String>();
+    _backendAddressDiscovery(completer);
+    return completer.future;
+  }
+
+  void _backendAddressDiscovery(Completer<String> completer) async {
+    const String serviceName = '_http._tcp.local';
+    const String targetName = 'foi-air-adaptiq-il._http._tcp.local';
+
+    var client = MDnsClient();
+
+    log('Start MDNS discovery');
+    await client.start();
+
+    await for (final PtrResourceRecord ptr in client.lookup<PtrResourceRecord>(ResourceRecordQuery.serverPointer(serviceName))) {
+      await for (final SrvResourceRecord srv in client.lookup<SrvResourceRecord>(ResourceRecordQuery.service(ptr.domainName))) {
+        await for (final IPAddressResourceRecord ip in client.lookup<IPAddressResourceRecord>(ResourceRecordQuery.addressIPv4(srv.target))) {
+          if (srv.target == targetName) {
+            if (completer.isCompleted == false) {
+              log('Found backend address: ${ip.address.address}');
+              completer.complete(ip.address.address);
+            }
+          }
+        }
+      }
+    }
+
+    client.stop();
+    log('Stop MDNS Discovery');
+  }
+}
