b1d8f9072578bcc7aa9465b31284ea3e19a13f2a
jmojzes21 <jmojzes21@student.foi.hr>
2024-12-03 19:00:55 +0100

    Improve login UI, add logo and app title to login page

--- il_app/lib/ui/pages/login_page.dart
+++ il_app/lib/ui/pages/login_page.dart
@@ -14,61 +14,93 @@ class LoginPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Login'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(40),
-        child: ChangeNotifierProvider(
-          create: (context) => LoginPageViewModel(
-            navigateToHomePage: () => context.pushReplacement('/home'),
-          ),
-          child: Consumer<LoginPageViewModel>(
-            builder: (context, model, child) {
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextField(
-                    controller: model.tcUsername,
-                    decoration: const InputDecoration(
-                      label: Text('Username'),
-                      isDense: true,
-                      border: OutlineInputBorder(),
-                    ),
-                    autocorrect: false,
-                    enableSuggestions: false,
-                    textInputAction: TextInputAction.next,
-                  ),
-                  const SizedBox(height: 20),
-                  TextField(
-                    controller: model.tcPassword,
-                    decoration: InputDecoration(
-                      label: const Text('Password'),
-                      isDense: true,
-                      border: const OutlineInputBorder(),
-                      suffixIcon: IconButton(
-                        onPressed: () => model.togglePasswordVisibility(),
-                        icon: !model.showPassword ? const FaIcon(FontAwesomeIcons.solidEye) : const FaIcon(FontAwesomeIcons.solidEyeSlash),
-                      ),
-                    ),
-                    obscureText: !model.showPassword,
-                    autocorrect: false,
-                    enableSuggestions: false,
-                    textInputAction: TextInputAction.done,
-                  ),
-                  if (model.message != null)
-                    MessageCard(
-                      message: model.message!,
-                      onClose: () => model.clearMessage(),
-                    ),
-                  const SizedBox(height: 40),
-                  FilledButton(
-                    onPressed: () => model.login(),
-                    child: const Text('Log in'),
-                  ),
-                ],
-              );
-            },
-          ),
+      body: ChangeNotifierProvider(
+        create: (context) => LoginPageViewModel(
+          navigateToHomePage: () => context.pushReplacement('/home'),
         ),
+        child: buildBody(),
       ),
+    );
+  }
+
+  Widget buildBody() {
+    return Consumer<LoginPageViewModel>(
+      builder: (context, model, child) {
+        var body = Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const _AppLogoWidget(),
+            const SizedBox(height: 80),
+            TextField(
+              controller: model.tcUsername,
+              decoration: const InputDecoration(
+                label: Text('Username'),
+                isDense: true,
+                border: OutlineInputBorder(),
+              ),
+              autocorrect: false,
+              enableSuggestions: false,
+              textInputAction: TextInputAction.next,
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              controller: model.tcPassword,
+              decoration: InputDecoration(
+                label: const Text('Password'),
+                isDense: true,
+                border: const OutlineInputBorder(),
+                suffixIcon: IconButton(
+                  onPressed: () => model.togglePasswordVisibility(),
+                  icon: !model.showPassword ? const FaIcon(FontAwesomeIcons.solidEye) : const FaIcon(FontAwesomeIcons.solidEyeSlash),
+                ),
+              ),
+              obscureText: !model.showPassword,
+              autocorrect: false,
+              enableSuggestions: false,
+              textInputAction: TextInputAction.done,
+            ),
+            if (model.message != null)
+              MessageCard(
+                message: model.message!,
+                onClose: () => model.clearMessage(),
+              ),
+            const SizedBox(height: 40),
+            FilledButton(
+              onPressed: () => model.login(),
+              child: const Text('Log in'),
+            ),
+          ],
+        );
+
+        return Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(40),
+              child: body,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _AppLogoWidget extends StatelessWidget {
+  const _AppLogoWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Image.asset('assets/images/sick-mobilisis-logo.png'),
+        ),
+        const SizedBox(height: 10),
+        Text('Indoor Localization', style: Theme.of(context).textTheme.headlineLarge),
+      ],
     );
   }
 }
