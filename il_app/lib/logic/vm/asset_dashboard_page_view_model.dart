354b80616d0311543b74644fc7f85b63c40a3502
jmojzes21 <jmojzes21@student.foi.hr>
2024-12-05 12:41:50 +0100

    Create a template for different ways to show assets

--- il_app/lib/logic/vm/asset_dashboard_page_view_model.dart
+++ il_app/lib/logic/vm/asset_dashboard_page_view_model.dart
@@ -1,3 +1,45 @@
 import 'package:flutter/foundation.dart';
+import 'package:il_core/il_core.dart';
+import 'package:il_core/il_entities.dart';
 
-class AssetDashboardPageViewModel extends ChangeNotifier {}
+class AssetDashboardPageViewModel extends ChangeNotifier {
+  late final List<IAssetDisplayHandler> _displayHandlers;
+  late IAssetDisplayHandler _currentDisplayHandler;
+
+  List<Asset> assets = [];
+
+  AssetDashboardPageViewModel({
+    required List<IAssetDisplayHandler> displayHandlers,
+  }) {
+    _displayHandlers = displayHandlers;
+    _currentDisplayHandler = displayHandlers.first;
+
+    assets = [
+      Asset(id: 1, name: 'Asset 1', x: 0, y: 0, lastSync: DateTime.now(), active: true, floorMapId: 1),
+      Asset(id: 2, name: 'Asset 2', x: 0, y: 0, lastSync: DateTime.now(), active: true, floorMapId: 1),
+      Asset(id: 3, name: 'Asset 3', x: 0, y: 0, lastSync: DateTime.now(), active: true, floorMapId: 1),
+    ];
+  }
+
+  List<IAssetDisplayHandler> get displayHandlers => _displayHandlers;
+  IAssetDisplayHandler get currentDisplayHandler => _currentDisplayHandler;
+
+  void showAssets() {
+    _currentDisplayHandler.showAssets(assets);
+  }
+
+  void changeDisplayHandler(IAssetDisplayHandler handler) {
+    _currentDisplayHandler = handler;
+    notifyListeners();
+    showAssets();
+  }
+
+  @override
+  void dispose() {
+    for (var e in _displayHandlers) {
+      e.dispose();
+    }
+
+    super.dispose();
+  }
+}
--- il_app/lib/ui/pages/asset_dashboard_page.dart
+++ il_app/lib/ui/pages/asset_dashboard_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:il_app/logic/vm/asset_dashboard_page_view_model.dart';
 import 'package:il_app/ui/widgets/navigation_drawer.dart';
+import 'package:il_display_assets_map/il_display_assets_map.dart';
+import 'package:il_display_assets_table/il_display_assets_table.dart';
 import 'package:provider/provider.dart';
 
 class AssetDashboardPage extends StatelessWidget {
@@ -16,7 +18,12 @@ class AssetDashboardPage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(20),
         child: ChangeNotifierProvider(
-          create: (context) => AssetDashboardPageViewModel(),
+          create: (context) => AssetDashboardPageViewModel(
+            displayHandlers: [
+              MapAssetDisplayHandler(),
+              TableAssetDisplayHandler(),
+            ],
+          ),
           child: buildBody(),
         ),
       ),
@@ -27,7 +34,22 @@ class AssetDashboardPage extends StatelessWidget {
     return Consumer<AssetDashboardPageViewModel>(
       builder: (context, model, child) {
         return Column(
-          children: [],
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextButton(
+              onPressed: () => model.showAssets(),
+              child: const Text('Show assets'),
+            ),
+            const SizedBox(height: 20),
+            ...model.displayHandlers.map((e) {
+              return TextButton(
+                onPressed: () => model.changeDisplayHandler(e),
+                child: Text(e.getDisplayName()),
+              );
+            }),
+            const SizedBox(height: 20),
+            model.currentDisplayHandler.buildWidget(context),
+          ],
         );
       },
     );
