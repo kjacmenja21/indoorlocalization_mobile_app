eb00b2766ce3d4d5098503e41259597bc59e5000
jmojzes21 <jmojzes21@student.foi.hr>
2024-12-08 10:27:43 +0100

    Create asset service

--- il_ws/lib/il_ws.dart
+++ il_ws/lib/il_ws.dart
@@ -2,3 +2,5 @@ library;
 
 export 'src/services/authentication_service.dart';
 export 'src/misc/backend_discovery.dart';
+export 'src/services/asset_service.dart';
+export 'src/services/floor_map_service.dart';
