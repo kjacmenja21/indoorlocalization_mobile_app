74a72489bb78b1347575411300feac7804a3c239
jmojzes21 <jmojzes21@student.foi.hr>
2024-12-08 13:19:56 +0100

    Update fake asset and floor map services, add test svg

--- il_ws/lib/src/services/asset_service.dart
+++ il_ws/lib/src/services/asset_service.dart
@@ -8,19 +8,23 @@ abstract class IAssetService {
 class FakeAssetService implements IAssetService {
   @override
   Future<List<Asset>> getAllAssets() async {
-    return [
-      Asset(id: 1, name: 'Asset 1', x: 10, y: 10, lastSync: DateTime.now(), active: true, floorMapId: 1),
-      Asset(id: 2, name: 'Asset 2', x: 10, y: 10, lastSync: DateTime.now(), active: true, floorMapId: 1),
-      Asset(id: 3, name: 'Asset 3', x: 10, y: 10, lastSync: DateTime.now(), active: true, floorMapId: 1),
-    ];
+    return Future.delayed(Duration(milliseconds: 500), () {
+      return [
+        Asset(id: 1, name: 'Asset 1', x: 100, y: 100, lastSync: DateTime.now(), active: true, floorMapId: 1),
+        Asset(id: 2, name: 'Asset 2', x: 500, y: 500, lastSync: DateTime.now(), active: true, floorMapId: 1),
+        Asset(id: 3, name: 'Asset 3', x: 1000, y: 1000, lastSync: DateTime.now(), active: true, floorMapId: 1),
+      ];
+    });
   }
 
   @override
   Future<List<Asset>> getAssetsByFloorMap(int floorMapId) async {
-    return [
-      Asset(id: 1, name: 'Asset 1', x: 10, y: 10, lastSync: DateTime.now(), active: true, floorMapId: floorMapId),
-      Asset(id: 2, name: 'Asset 2', x: 10, y: 10, lastSync: DateTime.now(), active: true, floorMapId: floorMapId),
-      Asset(id: 3, name: 'Asset 3', x: 10, y: 10, lastSync: DateTime.now(), active: true, floorMapId: floorMapId),
-    ];
+    return Future.delayed(Duration(milliseconds: 500), () {
+      return [
+        Asset(id: 1, name: 'Asset 1', x: 100, y: 100, lastSync: DateTime.now(), active: true, floorMapId: floorMapId),
+        Asset(id: 2, name: 'Asset 2', x: 500, y: 500, lastSync: DateTime.now(), active: true, floorMapId: floorMapId),
+        Asset(id: 3, name: 'Asset 3', x: 1000, y: 1000, lastSync: DateTime.now(), active: true, floorMapId: floorMapId),
+      ];
+    });
   }
 }
--- il_ws/lib/src/services/floor_map_service.dart
+++ il_ws/lib/src/services/floor_map_service.dart
@@ -8,26 +8,39 @@ abstract class IFloorMapService {
 }
 
 class FakeFloorMapService implements IFloorMapService {
+  final _testSvg = '''
+  <svg id="air" viewBox="0 0 3000 2000" shape-rendering="geometricPrecision" text-rendering="geometricPrecision">
+    <rect id="rect1" width="240" height="1520" x="260" y="240" fill="none" stroke="#000000"/>
+    <rect id="rect2" width="1140" height="200" x="1400" y="240" fill="none" stroke="#000000"/>
+    <rect id="rect3" width="1140" height="200" x="1400" y="620" fill="none" stroke="#000000"/>
+    <rect id="rect4" width="1140" height="200" x="1400" y="1000" fill="none" stroke="#000000"/>
+  </svg>
+  ''';
+
   @override
   Future<List<FloorMap>> getAllFloorMaps() async {
-    return [
-      FloorMap(
-        id: 1,
-        name: 'Floor map 1',
-        trackingArea: Rect.fromLTWH(0, 0, 300, 200),
-        svgImage: '',
-      ),
-      FloorMap(
-        id: 2,
-        name: 'Floor map 2',
-        trackingArea: Rect.fromLTWH(0, 0, 300, 200),
-        svgImage: '',
-      ),
-    ];
+    return Future.delayed(Duration(milliseconds: 500), () {
+      return [
+        FloorMap(
+          id: 1,
+          name: 'Floor map 1',
+          trackingArea: Rect.fromLTWH(0, 0, 3000, 2000),
+          svgImage: _testSvg,
+        ),
+        FloorMap(
+          id: 2,
+          name: 'Floor map 2',
+          trackingArea: Rect.fromLTWH(0, 0, 3000, 2000),
+          svgImage: _testSvg,
+        ),
+      ];
+    });
   }
 
   @override
   Future<List<FloorMapZone>> getFloorMapZones(int floorMapId) async {
-    return [];
+    return Future.delayed(Duration(milliseconds: 500), () {
+      return [];
+    });
   }
 }
