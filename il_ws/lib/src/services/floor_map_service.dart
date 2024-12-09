dfc8dc50358da96550df7df5427cb3d4df5e8b6c
jmojzes21 <jmojzes21@student.foi.hr>
2024-12-09 11:59:13 +0100

    Update test svg in fake floor map service

--- il_ws/lib/src/services/floor_map_service.dart
+++ il_ws/lib/src/services/floor_map_service.dart
@@ -10,10 +10,10 @@ abstract class IFloorMapService {
 class FakeFloorMapService implements IFloorMapService {
   final _testSvg = '''
   <svg id="air" viewBox="0 0 3000 2000" shape-rendering="geometricPrecision" text-rendering="geometricPrecision">
-    <rect id="rect1" width="240" height="1520" x="260" y="240" fill="none" stroke="#000000"/>
-    <rect id="rect2" width="1140" height="200" x="1400" y="240" fill="none" stroke="#000000"/>
-    <rect id="rect3" width="1140" height="200" x="1400" y="620" fill="none" stroke="#000000"/>
-    <rect id="rect4" width="1140" height="200" x="1400" y="1000" fill="none" stroke="#000000"/>
+    <rect id="rect1" width="240" height="1520" x="260" y="240" fill="none" stroke="#000000" stroke-width="4"/>
+    <rect id="rect2" width="1140" height="200" x="1400" y="240" fill="none" stroke="#000000" stroke-width="4"/>
+    <rect id="rect3" width="1140" height="200" x="1400" y="620" fill="none" stroke="#000000" stroke-width="4"/>
+    <rect id="rect4" width="1140" height="200" x="1400" y="1000" fill="none" stroke="#000000" stroke-width="4"/>
   </svg>
   ''';
 
