c6ce726dd9667d954fe9851f48d368287bfcb65e
jmojzes21 <jmojzes21@student.foi.hr>
2024-12-12 10:05:07 +0100

    Update session service to store user refresh token

--- il_app/lib/logic/services/session_service.dart
+++ il_app/lib/logic/services/session_service.dart
@@ -2,31 +2,41 @@ import 'package:il_core/il_entities.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 abstract class ISessionService {
-  Future<String?> loadSession();
+  Future<JwtToken?> loadSession();
   Future<void> saveSession(RegisteredUser registeredUser);
   Future<void> deleteSession();
 }
 
 class SessionService implements ISessionService {
-  static const String _userTokenKey = 'userJwtToken';
+  static const String _refreshTokenKey = 'user-refresh-token';
 
   @override
-  Future<String?> loadSession() async {
+  Future<JwtToken?> loadSession() async {
     var prefs = await SharedPreferences.getInstance();
 
-    String? jwtToken = prefs.getString(_userTokenKey);
-    return jwtToken;
+    String? token = prefs.getString(_refreshTokenKey);
+    if (token == null) {
+      return null;
+    }
+
+    try {
+      return JwtToken.decode(token);
+    } catch (_) {
+      return null;
+    }
   }
 
   @override
   Future<void> saveSession(RegisteredUser registeredUser) async {
+    String token = registeredUser.refreshToken.value;
+
     var prefs = await SharedPreferences.getInstance();
-    await prefs.setString(_userTokenKey, registeredUser.jwtToken);
+    await prefs.setString(_refreshTokenKey, token);
   }
 
   @override
   Future<void> deleteSession() async {
     var prefs = await SharedPreferences.getInstance();
-    await prefs.remove(_userTokenKey);
+    await prefs.remove(_refreshTokenKey);
   }
 }
