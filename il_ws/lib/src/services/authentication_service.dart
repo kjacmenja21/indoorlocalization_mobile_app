36c6836bff0556c6c4ae78069f7e30b4c3afb466
jmojzes21 <jmojzes21@student.foi.hr>
2024-12-12 17:46:34 +0100

    Fix autologin

--- il_ws/lib/src/services/authentication_service.dart
+++ il_ws/lib/src/services/authentication_service.dart
@@ -31,13 +31,14 @@ class AuthenticationService extends WebService implements IAuthenticationService
 
   @override
   Future<RegisteredUser> renewSession(JwtToken refreshToken) async {
+    var cookie = 'refresh-token=${refreshToken.value}';
+
     var response = await httpPost(
       path: '/api/v1/auth/autologin',
-      body: {'refresh_token': refreshToken.value},
-      contentType: 'application/x-www-form-urlencoded',
+      headers: {'Cookie': cookie},
     );
 
-    var user = User.fromJson(response['user']);
+    var user = User.fromJson(response['data']);
     var accessToken = JwtToken.decode(response['access_token']);
     var newRefreshToken = JwtToken.decode(response['refresh_token']);
 
