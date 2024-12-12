ab2b6f6d2cb76d8c621a20842e4bdceddf6096b4
jmojzes21 <jmojzes21@student.foi.hr>
2024-12-12 10:04:30 +0100

    Update auth services to use access and refresh token

--- il_basic_auth/lib/src/auto_login_handler.dart
+++ il_basic_auth/lib/src/auto_login_handler.dart
@@ -1,11 +1,12 @@
 import 'package:il_core/il_core.dart';
+import 'package:il_core/il_entities.dart';
 import 'package:il_core/il_exceptions.dart';
 import 'package:il_ws/il_ws.dart';
 
 class AutoLoginToken implements ILoginToken {
-  final String jwtToken;
+  final JwtToken refreshToken;
 
-  AutoLoginToken(this.jwtToken);
+  AutoLoginToken(this.refreshToken);
 }
 
 class AutoLoginHandler extends ILoginHandler {
@@ -25,7 +26,7 @@ class AutoLoginHandler extends ILoginHandler {
     AutoLoginToken token = baseToken as AutoLoginToken;
 
     try {
-      var registeredUser = await authService.renewSession(token.jwtToken);
+      var registeredUser = await authService.renewSession(token.refreshToken);
 
       loginListener.onSuccessfulLogin(registeredUser);
     } on AppException catch (e) {
--- il_ws/lib/src/services/authentication_service.dart
+++ il_ws/lib/src/services/authentication_service.dart
@@ -3,38 +3,48 @@ import 'package:il_ws/src/services/web_service.dart';
 
 abstract class IAuthenticationService {
   Future<RegisteredUser> login(String username, String password);
-  Future<RegisteredUser> renewSession(String token);
+  Future<RegisteredUser> renewSession(JwtToken refreshToken);
 }
 
 class AuthenticationService extends WebService implements IAuthenticationService {
   @override
   Future<RegisteredUser> login(String username, String password) async {
-    var response = await httpPost(path: '/api/login', body: {
-      'username': username,
-      'password': password,
-    });
+    var response = await httpPost(
+      path: '/api/v1/auth/login',
+      body: {
+        'username': username,
+        'password': password,
+      },
+      contentType: 'application/x-www-form-urlencoded',
+    );
 
     var user = User.fromJson(response['user']);
-    var jwtToken = response['token'] as String;
+    var accessToken = JwtToken.decode(response['access_token']);
+    var refreshToken = JwtToken.decode(response['refresh_token']);
 
     return RegisteredUser(
       user: user,
-      jwtToken: jwtToken,
+      accessToken: accessToken,
+      refreshToken: refreshToken,
     );
   }
 
   @override
-  Future<RegisteredUser> renewSession(String token) async {
-    var response = await httpPost(path: '/api/autologin', body: {
-      'token': token,
-    });
+  Future<RegisteredUser> renewSession(JwtToken refreshToken) async {
+    var response = await httpPost(
+      path: '/api/v1/auth/autologin',
+      body: {'refresh_token': refreshToken.value},
+      contentType: 'application/x-www-form-urlencoded',
+    );
 
     var user = User.fromJson(response['user']);
-    var jwtToken = response['token'] as String;
+    var accessToken = JwtToken.decode(response['access_token']);
+    var newRefreshToken = JwtToken.decode(response['refresh_token']);
 
     return RegisteredUser(
       user: user,
-      jwtToken: jwtToken,
+      accessToken: accessToken,
+      refreshToken: newRefreshToken,
     );
   }
 }
