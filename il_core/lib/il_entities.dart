71af285df56eb8c1fa70663dfb53e2148d2c6749
jmojzes21 <jmojzes21@student.foi.hr>
2024-12-23 09:21:20 +0100

    Create asset position history entity

--- il_core/lib/il_entities.dart
+++ il_core/lib/il_entities.dart
@@ -5,6 +5,7 @@ export 'src/entities/user_role.dart';
 export 'src/entities/asset.dart';
 export 'src/entities/floor_map.dart';
 export 'src/entities/floor_map_zone.dart';
+export 'src/entities/asset_position_history.dart';
 
 export 'src/models/registered_user.dart';
 export 'src/models/asset_location.dart';
