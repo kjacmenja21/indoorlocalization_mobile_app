714caf5c907ff3ba32c1652cc38b8fdcb4933f09
jmojzes21 <jmojzes21@student.foi.hr>
2024-12-23 17:07:21 +0100

    Create heatmap report generator

---
+++ il_app/lib/ui/widgets/reports/asset_report_generators.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:il_app/ui/widgets/reports/heatmap_report.dart';
+import 'package:il_core/il_core.dart';
+
+class AssetHeatmapReportGenerator implements IAssetReportGenerator {
+  @override
+  Widget buildDisplayWidget({required VoidCallback onTap}) {
+    return FilledButton(
+      onPressed: () => onTap(),
+      child: const Text('Generate heatmap'),
+    );
+  }
+
+  @override
+  Widget buildWidget(BuildContext context, data) {
+    return HeatmapReportWidget();
+  }
+
+  @override
+  Future generateData({required int assetId, required int floorMapId, required DateTime startDate, required DateTime endDate}) async {
+    return 0;
+  }
+}
---
+++ il_app/lib/ui/widgets/reports/heatmap_report.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HeatmapReportWidget extends StatefulWidget {
+  const HeatmapReportWidget({super.key});
+
+  @override
+  State<HeatmapReportWidget> createState() => _HeatmapReportWidgetState();
+}
+
+class _HeatmapReportWidgetState extends State<HeatmapReportWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
