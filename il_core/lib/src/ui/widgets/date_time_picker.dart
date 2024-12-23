0a4981f6c22605342aac547db4cea790fcd9bbd9
jmojzes21 <jmojzes21@student.foi.hr>
2024-12-23 17:06:28 +0100

    Create date time picker widget

---
+++ il_core/lib/src/ui/widgets/date_time_picker.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class DateTimePicker extends StatelessWidget {
+  final dateFormat = DateFormat('dd.MM.yyyy.  HH:mm');
+
+  final DateTime? value;
+  final void Function(DateTime value) onUpdate;
+
+  DateTimePicker({
+    super.key,
+    required this.value,
+    required this.onUpdate,
+  });
+
+  String getDateTimeText() {
+    if (value != null) {
+      return dateFormat.format(value!);
+    } else {
+      return 'Select date';
+    }
+  }
+
+  Future<void> selectDate(BuildContext context) async {
+    DateTime? result = await showDatePicker(
+      context: context,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(3000),
+    );
+
+    if (result != null) {
+      DateTime value = this.value ?? DateTime.now();
+
+      int hour = value.hour;
+      int minute = value.minute;
+
+      onUpdate(DateTime(result.year, result.month, result.day, hour, minute));
+    }
+  }
+
+  Future<void> selectTime(BuildContext context) async {
+    TimeOfDay? result = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+      initialEntryMode: TimePickerEntryMode.dialOnly,
+    );
+
+    if (result != null) {
+      DateTime value = this.value ?? DateTime.now();
+
+      int year = value.year;
+      int month = value.month;
+      int day = value.day;
+
+      onUpdate(DateTime(year, month, day, result.hour, result.minute));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(0),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Row(
+          children: [
+            Text(getDateTimeText(), style: Theme.of(context).textTheme.bodyLarge),
+            Spacer(),
+            IconButton(
+              onPressed: () => selectDate(context),
+              icon: const Icon(Icons.calendar_month),
+            ),
+            IconButton(
+              onPressed: () => selectTime(context),
+              icon: const Icon(Icons.timer),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
