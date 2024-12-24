afeec5ea9c6b4b2cc0f87ff8818e71f54397e4d7
jmojzes21 <jmojzes21@student.foi.hr>
2024-12-24 10:52:57 +0100

    Create asset heatmap data model

---
+++ features/il_reports/lib/src/models/asset_heatmap_data.dart
@@ -0,0 +1,35 @@
+import 'dart:ui';
+
+import 'package:il_core/il_entities.dart';
+
+class AssetHeatmapCell {
+  double minutes = 0;
+}
+
+class AssetHeatmapData {
+  Asset asset;
+  late FloorMap floorMap;
+
+  late Size mapSize = Size.zero;
+  Size cellSize = Size.zero;
+
+  int rows = 0;
+  int columns = 0;
+
+  List<AssetHeatmapCell> cells = [];
+
+  AssetHeatmapData(this.asset) {
+    floorMap = asset.floorMap!;
+  }
+
+  AssetHeatmapCell cellAt(int x, int y) {
+    return cells[x + y * columns];
+  }
+
+  AssetHeatmapCell cellFromPosition(double x, double y) {
+    int cx = (x / cellSize.width).floor();
+    int cy = (y / cellSize.height).floor();
+
+    return cells[cx + cy * columns];
+  }
+}
