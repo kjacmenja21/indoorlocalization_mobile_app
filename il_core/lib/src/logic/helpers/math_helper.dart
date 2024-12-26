94a4191ec63167ed1a3ef9e24963bd30e13c881c
jmojzes21 <jmojzes21@student.foi.hr>
2024-12-26 12:52:41 +0100

    Add lerpInt and lerpColor to MathHelper

--- il_core/lib/src/logic/helpers/math_helper.dart
+++ il_core/lib/src/logic/helpers/math_helper.dart
@@ -1,5 +1,20 @@
+import 'dart:ui';
+
 class MathHelper {
+  static int lerpInt(int a, int b, double t) {
+    return (a * (1.0 - t) + b * t).round();
+  }
+
   static double lerpDouble(double a, double b, double t) {
     return a * (1.0 - t) + b * t;
+  }
+
+  static Color lerpColor(Color a, Color b, double t) {
+    return Color.fromARGB(
+      lerpInt(a.alpha, b.alpha, t),
+      lerpInt(a.red, b.red, t),
+      lerpInt(a.green, b.green, t),
+      lerpInt(a.blue, b.blue, t),
+    );
   }
 }
