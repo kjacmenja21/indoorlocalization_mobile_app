7f52b3b295afe678d17dd782c8631f052f562e4f
jmojzes21 <jmojzes21@student.foi.hr>
2024-12-30 09:16:30 +0100

    Add zone to asset zone history entity

--- il_core/lib/src/entities/asset_zone_history.dart
+++ il_core/lib/src/entities/asset_zone_history.dart
@@ -1,3 +1,5 @@
+import 'package:il_core/il_entities.dart';
+
 class AssetZoneHistory {
   int id;
   int assetId;
@@ -6,12 +8,15 @@ class AssetZoneHistory {
   DateTime enterDateTime;
   DateTime exitDateTime;
 
+  FloorMapZone? zone;
+
   AssetZoneHistory({
     required this.id,
     required this.assetId,
     required this.zoneId,
     required this.enterDateTime,
     required this.exitDateTime,
+    this.zone,
   });
 
   Duration get retentionTime {
