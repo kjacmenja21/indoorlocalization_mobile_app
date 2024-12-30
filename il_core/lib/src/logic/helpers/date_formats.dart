d2668b5ecf63233736b932c76d77a1fb2c87b3da
jmojzes21 <jmojzes21@student.foi.hr>
2024-12-30 09:16:09 +0100

    Add formatDuration to DateFormats helper

--- il_core/lib/src/logic/helpers/date_formats.dart
+++ il_core/lib/src/logic/helpers/date_formats.dart
@@ -2,4 +2,21 @@ import 'package:intl/intl.dart';
 
 class DateFormats {
   static final dateTime = DateFormat('dd.MM.yyyy.  HH:mm');
+
+  static String formatDuration(Duration duration) {
+    int totalMinutes = duration.inMinutes;
+
+    int hours = totalMinutes ~/ 60;
+    int minutes = totalMinutes - hours * 60;
+
+    if (hours == 0) {
+      return '${minutes}min';
+    }
+
+    if (minutes == 0) {
+      return '${hours}h';
+    }
+
+    return '${hours}h ${minutes}min';
+  }
 }
