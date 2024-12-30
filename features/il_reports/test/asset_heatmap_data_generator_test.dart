ff69b500ceb5aaa77a667ef949655774e7ee1ae6
jmojzes21 <jmojzes21@student.foi.hr>
2024-12-30 19:20:32 +0100

    Update asset heatmap tests

--- features/il_reports/test/asset_heatmap_data_generator_test.dart
+++ features/il_reports/test/asset_heatmap_data_generator_test.dart
@@ -5,13 +5,6 @@ import 'package:il_reports/src/logic/asset_heatmap_data_generator.dart';
 import 'package:il_reports/src/models/asset_heatmap_data.dart';
 
 void main() {
-  var posHistory = <String, AssetPositionHistory>{
-    't1': createPosHistory('10:00', 20, 20),
-    't2': createPosHistory('12:30', 40, 40),
-    't3': createPosHistory('12:00', 120, 20),
-    't4': createPosHistory('10:40', 320, 20),
-  };
-
   var mapSize = const Size(10000, 10000);
   var cellSize = const Size(100, 100);
 
@@ -21,10 +14,10 @@ void main() {
     gradient: createGradient(),
   );
 
-  group('getTimeDifferenceMinutes', () {
-    test('getTimeDifferenceMinutes, given 2 pos history, time diff is correct', () {
-      var t1 = posHistory['t1']!;
-      var t2 = posHistory['t2']!;
+  group('getTimeDifferenceMinutes()', () {
+    test('given 2 pos history, time diff is correct', () {
+      var t1 = createPosHistory('10:00', 20, 20);
+      var t2 = createPosHistory('12:30', 40, 40);
 
       double minutes = generator.getTimeDifferenceMinutes(t1, t2);
 
@@ -32,7 +25,7 @@ void main() {
     });
   });
 
-  group('addTimeToCells', () {
+  group('addTimeToCells()', () {
     late AssetHeatmapData data;
 
     setUp(() {
@@ -46,10 +39,10 @@ void main() {
     });
 
     test(
-      'addTimeToCells, given 2 pos history in same cell, total period is added to cell',
+      'given 2 pos history in same cell, total period is added to cell',
       () {
-        var t1 = posHistory['t1']!;
-        var t2 = posHistory['t2']!;
+        var t1 = createPosHistory('10:00', 20, 20);
+        var t2 = createPosHistory('12:30', 40, 40);
 
         var c1 = data.cellFromPosition(t1.x, t1.y);
         var c2 = data.cellFromPosition(t2.x, t2.y);
@@ -62,10 +55,10 @@ void main() {
     );
 
     test(
-      'addTimeToCells, given 2 pos history in neighboring cells, part of period is added to each cell',
+      'given 2 pos history in neighboring cells, part of period is added to each cell',
       () {
-        var t1 = posHistory['t1']!;
-        var t2 = posHistory['t3']!;
+        var t1 = createPosHistory('10:00', 20, 20);
+        var t2 = createPosHistory('12:00', 120, 20);
 
         var c1 = data.cellFromPosition(t1.x, t1.y);
         var c2 = data.cellFromPosition(t2.x, t2.y);
@@ -78,10 +71,10 @@ void main() {
     );
 
     test(
-      'addTimeToCells, given 2 pos history in far cells, part of period is added to each cell',
+      'given 2 pos history in far cells, part of period is added to each cell',
       () {
-        var t1 = posHistory['t1']!;
-        var t2 = posHistory['t4']!;
+        var t1 = createPosHistory('10:00', 20, 20);
+        var t2 = createPosHistory('10:40', 320, 20);
 
         var c1 = data.cellFromPosition(t1.x, t1.y);
         var c2 = data.cellFromPosition(t2.x, t2.y);
@@ -96,7 +89,7 @@ void main() {
     );
   });
 
-  group('calculateCellPercentage', () {
+  group('calculateCellPercentage()', () {
     late AssetHeatmapData data;
 
     setUp(() {
@@ -109,7 +102,7 @@ void main() {
       data.generateCells(generator.cellSize);
     });
 
-    test('calculateCellPercentage, given minutes, calculate cell percentage', () {
+    test('given minutes, calculate cell percentage', () {
       var c1 = data.cellAt(0, 0);
       var c2 = data.cellAt(20, 30);
       var c3 = data.cellAt(50, 50);
@@ -126,6 +119,52 @@ void main() {
       expect(c2.percentage, 0.2);
       expect(c3.percentage, 0.8);
       expect(c4.percentage, 1);
+    });
+  });
+
+  group('generate()', () {
+    test('given pos history 1, generate heatmap data', () {
+      generator.positionHistory = [
+        createPosHistory('10:00', 20, 20),
+        createPosHistory('10:40', 30, 30),
+      ];
+
+      var data = generator.generate();
+
+      expect(data.cellAt(0, 0).minutes, 40);
+    });
+
+    test('given pos history 2, generate heatmap data', () {
+      generator.positionHistory = [
+        createPosHistory('10:00', 20, 20),
+        createPosHistory('10:40', 320, 20),
+      ];
+
+      var data = generator.generate();
+
+      expect(data.cellAt(0, 0).minutes, 10);
+      expect(data.cellAt(1, 0).minutes, 10);
+      expect(data.cellAt(2, 0).minutes, 10);
+      expect(data.cellAt(3, 0).minutes, 10);
+    });
+
+    test('given pos history 3, generate heatmap data', () {
+      generator.positionHistory = [
+        createPosHistory('10:00', 20, 20),
+        createPosHistory('10:40', 320, 20),
+        createPosHistory('11:00', 340, 50),
+        createPosHistory('12:00', 340, 110),
+        createPosHistory('12:30', 340, 250),
+      ];
+
+      var data = generator.generate();
+
+      expect(data.cellAt(0, 0).minutes, 10);
+      expect(data.cellAt(1, 0).minutes, 10);
+      expect(data.cellAt(2, 0).minutes, 10);
+      expect(data.cellAt(3, 0).minutes, 10 + 20 + 30);
+      expect(data.cellAt(3, 1).minutes, 30 + 15);
+      expect(data.cellAt(3, 2).minutes, 15);
     });
   });
 }
