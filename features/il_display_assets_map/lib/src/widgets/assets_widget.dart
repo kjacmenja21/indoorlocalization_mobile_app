140851d8b1adaa787fd0419b9c0a607b390138b6
jmojzes21 <jmojzes21@student.foi.hr>
2024-12-31 14:02:30 +0100

    Open asset details dialog

--- features/il_display_assets_map/lib/src/widgets/assets_widget.dart
+++ features/il_display_assets_map/lib/src/widgets/assets_widget.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:il_core/il_core.dart';
+import 'package:il_core/il_entities.dart';
+import 'package:il_core/il_helpers.dart';
 import 'package:il_core/il_widgets.dart';
+import 'package:il_display_assets_map/src/widgets/asset_details_dialog.dart';
 import 'package:il_display_assets_map/src/widgets/assets_painter.dart';
 import 'package:il_display_assets_map/src/widgets/floor_map_painter.dart';
 
@@ -12,6 +15,38 @@ class AssetsWidget extends StatelessWidget {
     required this.model,
   });
 
+  void openAssetDetailsDialog(BuildContext context, Asset asset) {
+    showDialog(
+      context: context,
+      builder: (context) => AssetDetailsDialog(
+        model: model,
+        asset: asset,
+      ),
+    );
+  }
+
+  void onDoubleTapDown(BuildContext context, TapDownDetails details, Matrix4 transform) {
+    var assets = model.assets;
+    var scale = transform.getMaxScaleOnAxis();
+    var translation = transform.getTranslation();
+
+    var geometryHelper = GeometryHelper();
+    var tapPosition = details.localPosition;
+
+    for (var asset in assets) {
+      double x = asset.x * scale + translation.x;
+      double y = asset.y * scale + translation.y;
+      var assetPosition = Offset(x, y);
+
+      double distance = geometryHelper.distanceBetweenPoints(assetPosition, tapPosition);
+
+      if (distance <= 30) {
+        openAssetDetailsDialog(context, asset);
+        break;
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListenableBuilder(
@@ -22,6 +57,7 @@ class AssetsWidget extends StatelessWidget {
 
         return FloorMapWidget(
           floorMap: floorMap,
+          onDoubleTapDown: (details, transform) => onDoubleTapDown(context, details, transform),
           backgroundBuilder: (svg) {
             return CustomPaint(
               willChange: false,
