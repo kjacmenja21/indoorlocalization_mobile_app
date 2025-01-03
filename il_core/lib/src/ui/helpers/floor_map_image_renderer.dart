d7dfaac3a6336c96dc20c33e5e821e525e5e5976
jmojzes21 <jmojzes21@student.foi.hr>
2025-01-03 17:42:17 +0100

    Create floor map raster image renderer

--- il_core/lib/src/ui/helpers/floor_map_image_renderer.dart
+++ il_core/lib/src/ui/helpers/floor_map_image_renderer.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:il_core/il_entities.dart';
+import 'package:il_core/src/ui/helpers/floor_map_raster_image_renderer.dart';
 import 'package:il_core/src/ui/helpers/floor_map_svg_image_renderer.dart';
 
 abstract class IFloorMapImageRenderer {
@@ -10,6 +11,23 @@ abstract class IFloorMapImageRenderer {
   void dispose();
 
   factory IFloorMapImageRenderer.fromFloorMap(FloorMap floorMap) {
-    return FloorMapSvgImageRenderer(floorMap);
+    if (floorMap.image == null) {
+      throw ArgumentError('Floor map image is null');
+    }
+
+    if (floorMap.imageType == null) {
+      throw ArgumentError('Floor map image type is null');
+    }
+
+    switch (floorMap.imageType) {
+      case 'svg':
+        return FloorMapSvgImageRenderer(floorMap);
+
+      case 'png':
+      case 'jpg':
+        return FloorMapRasterImageRenderer(floorMap);
+    }
+
+    throw ArgumentError('Floor map image type ${floorMap.imageType} is not supported');
   }
 }
