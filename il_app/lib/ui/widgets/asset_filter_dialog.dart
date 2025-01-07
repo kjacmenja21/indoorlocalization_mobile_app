d1dc34babb1df0ac13e511870f224727c8272503
jmojzes21 <jmojzes21@student.foi.hr>
2025-01-07 13:55:41 +0100

    Use fullscreen dialog for AssetFilterDialog

--- il_app/lib/ui/widgets/asset_filter_dialog.dart
+++ il_app/lib/ui/widgets/asset_filter_dialog.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:il_app/ui/widgets/fullscreen_dialog.dart';
 import 'package:il_core/il_entities.dart';
 import 'package:il_core/il_widgets.dart';
 
@@ -85,62 +86,63 @@ class _AssetFilterDialogState extends State<AssetFilterDialog> {
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      title: const Text('Filter assets'),
-      insetPadding: const EdgeInsets.all(50),
-      content: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            CustomSearchBar(controller: tcSearch),
-            const SizedBox(height: 20),
-            Expanded(
-              child: SingleChildScrollView(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: currentAssets.map((asset) {
-                    return ListTile(
-                      title: Text(asset.name),
-                      leading: const FaIcon(FontAwesomeIcons.box),
-                      trailing: IconButton(
-                        onPressed: () => changeVisibility(asset),
-                        icon: getVisibilityIcon(asset.visible),
-                      ),
-                    );
-                  }).toList(),
-                ),
+    return FullscreenDialog(
+      title: 'Filter assets',
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          CustomSearchBar(controller: tcSearch),
+          const SizedBox(height: 20),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: currentAssets.map((asset) {
+                  return ListTile(
+                    title: Text(asset.name),
+                    leading: const FaIcon(FontAwesomeIcons.box),
+                    trailing: IconButton(
+                      onPressed: () => changeVisibility(asset),
+                      icon: getVisibilityIcon(asset.visible),
+                    ),
+                  );
+                }).toList(),
               ),
             ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                TextButton(
-                  onPressed: () => onHideAll(),
-                  child: const Text('Hide all'),
-                ),
-                const SizedBox(width: 10),
-                TextButton(
-                  onPressed: () => onShowAll(),
-                  child: const Text('Show all'),
-                ),
-              ],
-            ),
-          ],
-        ),
+          ),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                onPressed: () => onHideAll(),
+                child: const Text('Hide all'),
+              ),
+              const SizedBox(width: 10),
+              TextButton(
+                onPressed: () => onShowAll(),
+                child: const Text('Show all'),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                onPressed: () => onCancel(),
+                child: const Text('Cancel'),
+              ),
+              const SizedBox(width: 10),
+              FilledButton(
+                onPressed: () => onApply(),
+                child: const Text('Ok'),
+              ),
+            ],
+          ),
+        ],
       ),
-      actions: [
-        TextButton(
-          onPressed: () => onCancel(),
-          child: const Text('Cancel'),
-        ),
-        FilledButton(
-          onPressed: () => onApply(),
-          child: const Text('Ok'),
-        ),
-      ],
     );
   }
 
