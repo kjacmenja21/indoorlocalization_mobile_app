5a244ffaa36fa5972442cbee6030b1fac5e56b72
jmojzes21 <jmojzes21@student.foi.hr>
2025-01-09 10:58:49 +0100

    Minor changes to AssetsChangeNotifier

--- il_core/lib/src/logic/display_assets/assets_change_notifier.dart
+++ il_core/lib/src/logic/display_assets/assets_change_notifier.dart
@@ -2,27 +2,24 @@ import 'package:flutter/foundation.dart';
 import 'package:il_core/il_entities.dart';
 
 class AssetsChangeNotifier extends ChangeNotifier {
-  final FloorMap _floorMap;
-  List<Asset> _assets = [];
+  final FloorMap floorMap;
+  List<Asset> assets = [];
 
-  AssetsChangeNotifier(FloorMap floorMap) : _floorMap = floorMap;
+  AssetsChangeNotifier(this.floorMap);
 
   void setAssets(List<Asset> assets) {
-    _assets = assets;
+    assets = assets;
     notifyListeners();
   }
 
   bool updateAssetLocation(AssetLocation location) {
-    int i = _assets.indexWhere((e) => e.id == location.id);
+    int i = assets.indexWhere((e) => e.id == location.id);
     if (i == -1) {
       return false;
     }
 
-    _assets[i].updateLocation(location);
+    assets[i].updateLocation(location);
     notifyListeners();
     return true;
   }
-
-  FloorMap get floorMap => _floorMap;
-  List<Asset> get assets => _assets;
 }
