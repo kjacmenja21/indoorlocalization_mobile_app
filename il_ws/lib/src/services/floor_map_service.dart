77e1e41ced09af48a4e39ccb6953be25e2283f85
jmojzes21 <jmojzes21@student.foi.hr>
2025-01-15 09:37:20 +0100

    Implement in-memory floor maps cache

--- il_ws/lib/src/services/floor_map_service.dart
+++ il_ws/lib/src/services/floor_map_service.dart
@@ -7,8 +7,17 @@ abstract class IFloorMapService {
 }
 
 class FloorMapService extends WebService implements IFloorMapService {
+  static List<FloorMap> _cachedFloorMaps = [];
+  static DateTime? _floorMapsCacheDate;
+
   @override
   Future<List<FloorMap>> getAllFloorMaps() async {
+    List<FloorMap>? floorMaps = _loadCachedFloorMaps();
+
+    if (floorMaps != null) {
+      return floorMaps;
+    }
+
     var response = await httpGet(
       path: '/api/v1/floor-maps/',
       queryParameters: {
@@ -17,11 +26,14 @@ class FloorMapService extends WebService implements IFloorMapService {
       },
     );
 
-    var floorMaps = response['page'] as List<dynamic>;
+    var floorMapsJson = response['page'] as List<dynamic>;
 
-    return floorMaps.map((e) {
+    floorMaps = floorMapsJson.map((e) {
       return FloorMap.fromJson(e);
     }).toList();
+
+    _saveFloorMaps(floorMaps);
+    return floorMaps;
   }
 
   @override
@@ -38,5 +50,25 @@ class FloorMapService extends WebService implements IFloorMapService {
     return zones.map((e) {
       return FloorMapZone.fromJson(e);
     }).toList();
+  }
+
+  void _saveFloorMaps(List<FloorMap> floorMaps) {
+    _cachedFloorMaps = [...floorMaps];
+    _floorMapsCacheDate = DateTime.now();
+  }
+
+  List<FloorMap>? _loadCachedFloorMaps() {
+    if (_cachedFloorMaps.isEmpty || _floorMapsCacheDate == null) {
+      return null;
+    }
+
+    var now = DateTime.now();
+    var cacheAge = now.difference(_floorMapsCacheDate!);
+
+    if (cacheAge.inMinutes >= 10) {
+      return null;
+    }
+
+    return [..._cachedFloorMaps];
   }
 }
