70e789bacfbfcabb8ebcea92774e314c666b1fd4
jmojzes21 <jmojzes21@student.foi.hr>
2025-02-01 14:14:59 +0100

    Fix parse jwt token

--- il_core/lib/src/models/jwt_token.dart
+++ il_core/lib/src/models/jwt_token.dart
@@ -22,9 +22,22 @@ class JwtToken {
 
   int _getExpirationTime(String value) {
     var parts = value.split('.');
-    var json = utf8.decode(base64Decode(parts[1]));
+    var json = utf8.decode(base64Decode(_base64Padded(parts[1])));
     var payload = jsonDecode(json);
 
     return payload['exp'];
+  }
+
+  String _base64Padded(String value) {
+    var lenght = value.length;
+
+    switch (lenght % 4) {
+      case 2:
+        return value.padRight(lenght + 2, '=');
+      case 3:
+        return value.padRight(lenght + 1, '=');
+      default:
+        return value;
+    }
   }
 }
