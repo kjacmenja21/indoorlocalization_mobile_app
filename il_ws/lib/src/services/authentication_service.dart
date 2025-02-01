4f7796aa050bcd0761f10ec804b24800e1ad008a
jmojzes21 <jmojzes21@student.foi.hr>
2025-02-01 10:19:55 +0100

    Refresh user session if access token expires

--- il_ws/lib/src/services/authentication_service.dart
+++ il_ws/lib/src/services/authentication_service.dart
@@ -33,22 +33,7 @@ class AuthenticationService extends WebService implements IAuthenticationService
 
   @override
   Future<RegisteredUser> renewSession(JwtToken refreshToken) async {
-    var cookie = 'refresh-token=${refreshToken.value}';
-
-    var response = await httpPost(
-      path: '/api/v1/auth/autologin',
-      headers: {'Cookie': cookie},
-    );
-
-    var user = User.fromJson(response['data']);
-    var accessToken = JwtToken.decode(response['access_token']);
-    var newRefreshToken = JwtToken.decode(response['refresh_token']);
-
-    return RegisteredUser(
-      user: user,
-      accessToken: accessToken,
-      refreshToken: newRefreshToken,
-    );
+    return renewUserSession(refreshToken);
   }
 
   @override
