4679aa0d5dddef78120c83d2297b40f77d4a23db
jmojzes21 <jmojzes21@student.foi.hr>
2025-02-01 10:19:19 +0100

    Add isExpired() to JwtToken

--- il_core/lib/src/models/jwt_token.dart
+++ il_core/lib/src/models/jwt_token.dart
@@ -1,9 +1,30 @@
+import 'dart:convert';
+
 class JwtToken {
   final String value;
 
-  JwtToken({required this.value});
+  late int _expirationTime;
+
+  JwtToken({required this.value}) {
+    _expirationTime = _getExpirationTime(value);
+  }
 
   factory JwtToken.decode(String token) {
     return JwtToken(value: token);
+  }
+
+  bool isExpired() {
+    DateTime now = DateTime.now();
+    int seconds = now.millisecondsSinceEpoch ~/ 1000;
+
+    return seconds >= _expirationTime;
+  }
+
+  int _getExpirationTime(String value) {
+    var parts = value.split('.');
+    var json = utf8.decode(base64Decode(parts[1]));
+    var payload = jsonDecode(json);
+
+    return payload['exp'];
   }
 }
